48bb59cd98aeda3ba2e96eb149cbb1670b93d54e
fatedier <512654112@qq.com>
2014-11-22 01:24:54 +0800

    Update epoll

--- python/socket/sk.py
+++ python/socket/sk.py
@@ -5,12 +5,13 @@ import socket
 import os
 import time
 import threading
+import sys
 
 def deal_cli():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    #sv_ip = '127.0.0.1'
-    sv_ip = '10.32.146.9'
+    sv_ip = '127.0.0.1'
+    #sv_ip = '10.32.146.9'
     port = 9999
 
     s.connect((sv_ip, port))
@@ -25,11 +26,14 @@ def deal_cli():
         except:
             break
         time.sleep(0.1)
+try:
+    n = 0
+    while n < 999:
+        t = threading.Thread(target=deal_cli)
+        t.start()
+        n += 1
 
-n = 0
-while n < 999:
-    t = threading.Thread(target=deal_cli)
-    t.start()
-    n += 1
-
-deal_cli()
+    deal_cli()
+except:
+    print('over')
+    sys.exit(-1)
--- unp/src/app/u_reflect_sv_epoll/dd.cpp
+++ unp/src/app/u_reflect_sv_epoll/dd.cpp
@@ -6,13 +6,16 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
-#include <poll.h>
+#include <sys/epoll.h>
 #include <arpa/inet.h>
 
 #define MAXLINE 1024
-#define MAXCLIENT 1024
+#define MAXCLIENT 5000
 
-int needPrint = 0;
+char ip[] = "0.0.0.0";
+int port = 9999;
+
+int needPrint = 1;
 
 int main()
 {
@@ -21,8 +24,8 @@ int main()
     struct sockaddr_in svaddr;
     memset(&svaddr, 0, sizeof(svaddr));
     svaddr.sin_family = AF_INET;
-    svaddr.sin_addr.s_addr = inet_addr("192.168.131.128");
-    svaddr.sin_port = htons(9999);
+    svaddr.sin_addr.s_addr = inet_addr(ip);
+    svaddr.sin_port = htons(port);
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     if (listenfd < 0) {
@@ -47,77 +50,64 @@ int main()
     }
 
     int confd;
-    int nReady;                     //每次poll返回后就绪的描述符个数
+    int nReady;                     //每次epoll返回后就绪的描述符个数
     char buf[MAXLINE];
-    struct pollfd client[MAXCLIENT];     //pollfd结构体数组
-    int nFdUse = 0;                 //pollfd数组中有效连接的个数
-    client[0].fd = listenfd;
-    client[0].events = POLLRDNORM;
-    nFdUse++;
-
-    for (int i=1; i<MAXCLIENT; i++) {
-        client[i].fd = -1;
-    }
-
+    int nFdUse = 0;
     struct sockaddr_in cliaddr;
     socklen_t len;
-    for (;;) {
-        nReady = poll(client, MAXCLIENT, -1);
-        //监听套接字处理
-        if (client[0].revents & POLLRDNORM) {
-            len = sizeof(cliaddr);
-            confd = accept(listenfd, (struct sockaddr *)&cliaddr, &len);
-            if (confd < 0) {
-                printf("accept error,%s\n", strerror(errno));
-            } else {
-                if (needPrint)
-                    printf("accept sockfd:%d allnumber:%d\n", confd, nFdUse + 1);
 
-                if (nFdUse + 1 > MAXCLIENT) {
-                    if (needPrint)
-                        printf("too many clients!!!\n");
-                    close(confd);
+    int epoll_fd;
+    epoll_fd = epoll_create(MAXCLIENT);
+    struct epoll_event event;
+    struct epoll_event events[MAXCLIENT];
+    event.data.fd = listenfd;
+    event.events = EPOLLIN | EPOLLET;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listenfd, &event);
+    for (;;) {
+        nReady = epoll_wait(epoll_fd, events, MAXCLIENT, -1);
+        for (int i=0; i<nReady; i++) {
+            //监听套接字处理
+            if (events[i].data.fd == listenfd) {
+                len = sizeof(cliaddr);
+                confd = accept(listenfd, (struct sockaddr *)&cliaddr, &len);
+                if (confd < 0) {
+                    printf("accept error,%s\n", strerror(errno));
                 } else {
-                    for (int i=0; i<MAXCLIENT; i++) {
-                        if (client[i].fd < 0) {
-                            client[i].fd = confd;
-                            client[i].events = POLLRDNORM;
-                            nFdUse++;
-                            break;
-                        }
+                    if (needPrint)
+                        printf("accept sockfd:%d allnumber:%d\n", confd, nFdUse + 1);
+
+                    if (nFdUse + 1 > MAXCLIENT) {
+                        if (needPrint)
+                            printf("too many clients!!!\n");
+                        close(confd);
+                    } else {
+                        event.data.fd = confd;
+                        event.events = EPOLLIN | EPOLLET;
+                        epoll_ctl(epoll_fd, EPOLL_CTL_ADD, confd, &event);
+                        nFdUse++;
                     }
                 }
-            }
-
-            if (--nReady <= 0)
-                continue;
-        }
-
-        //遍历与客户端连接的套接字的处理
-        for (int i=1; i<MAXCLIENT; i++) {
-            if (client[i].fd < 0)
-                continue;
-            if (client[i].revents & (POLLRDNORM | POLL_ERR)) {
+            } else if (events[i].events & EPOLLIN) {
+                //遍历与客户端连接的套接字的处理
                 int n;
-                if ((n = read(client[i].fd, buf, MAXLINE)) < 0) {
+                if ((n = read(events[i].data.fd, buf, MAXLINE)) < 0) {
                     printf("read error\n");
-                    close(client[i].fd);
-                    client[i].fd = -1;
+                    event.data.fd = events[i].data.fd;
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, &event);
+                    close(events[i].data.fd);
                     nFdUse--;
 
                 } else if (n == 0) {
                     if (needPrint)
                         printf("remote socket is closed\n");
-                    close(client[i].fd);
-                    client[i].fd = -1;
+                    event.data.fd = events[i].data.fd;
+                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, &event);
+                    close(events[i].data.fd);
                     nFdUse--;
                 }
                 else {
-                    write(client[i].fd, buf, n);
+                    write(events[i].data.fd, buf, n);
                 }
-
-                if (--nReady <= 0)
-                    break;
             }
         }
     }
