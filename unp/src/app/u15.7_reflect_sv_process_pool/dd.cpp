aa7ba890f03dbaa734b29d8a2739a26862f7d491
fatedier <512654112@qq.com>
2014-11-25 18:28:33 +0800

    Company 2014-11-25

--- unp/src/app/u15.7_reflect_sv_process_pool/dd.cpp
+++ unp/src/app/u15.7_reflect_sv_process_pool/dd.cpp
@@ -9,13 +9,24 @@
 #include <arpa/inet.h>
 
 #define MAXLINE 1024
+#define CHILDNUMBER 50
 
 char ip[] = "0.0.0.0";
 int port = 9999;
 
 int needprint = 0;
 
-//清除变成僵尸进程的子进程
+/* 子进程信息 */
+typedef struct
+{
+    int is_use;         /* 是否使用中 0 or 1*/
+    int child_num;      /* 子进程序号 */
+    pid_t pid;          /* 子进程pid */
+    int child_sock_fd;  /* 父子进程之间通信的unix域套接字描述符 */
+    int state;          /* 子进程状态 0表示空闲，1表示忙 */
+} child_t;
+
+/* 清除变成僵尸进程的子进程 */
 void clean_child(int signo)
 {
     pid_t pid;
@@ -27,7 +38,13 @@ void clean_child(int signo)
     return;
 }
 
-//处理连接的具体事务
+/* 子进程的主函数 */
+void child_main(int parent_sockfd, int child_num)
+{
+    
+}
+
+/* 子进程处理连接的具体事务 */
 void deal_socket(int sockfd)
 {
     char buf[MAXLINE];
@@ -45,8 +62,39 @@ again:
 
 int main()
 {
-    //绑定SIGCHLD信号的处理函数
+    /* 绑定SIGCHLD信号的处理函数 */
     signal(SIGCHLD, clean_child);
+
+    /* 子进程使用变量 */
+    int child_num;                      /* 子进程序号 父进程中赋值，在子进程中使用 */
+    int parent_sockfd;                  /* 和父进程通信的unix域套接字描述符 */
+
+    child_t  childs[CHILDNUMBER];       /* 子进程信息数组 */
+    int childs_run_num = 0;             /* 当前正在运行中的子进程个数 */
+
+    /* 预先创建一定个数的子进程，初始化子进程信息数组 */
+    for (int i=0; i<CHILDNUMBER; i++) {
+        child_num = i;                  /* 子进程记录下自己的序号 */
+        childs[i].child_num = i;
+
+        int sockfd[2];
+        socketpair(AF_LOCAL, SOCK_STREAM, 0, sockfd);   /* 创建两个连接在一起的unix域套接字 */
+        childs[i].child_sock_fd = sockfd[0];            /* 父进程使用sockfd[0]和子进程通信 */
+        parent_sockfd = sockfd[1];                      /* 子进程使用sockfd[1]和父进程通信 */
+        
+        childs[i].is_use = 1;
+        childs[i].state = 0;
+
+        pid_t pid = fork();
+        if (pid == 0) {                                 /* 子进程 */
+            child_main(parent_sockfd, child_num);
+            return 0;
+        } else if (pid > 0) {                           /* 父进程 */
+            childs[i].pid = pid;
+        } else {
+            printf("fork child %d error,%s\n", i, strerror(errno));
+        }
+    }
 
     int listenfd, confd;
 
@@ -83,26 +131,24 @@ int main()
     len = sizeof(cliaddr);
     for (;;) {
         confd = accept(listenfd, (struct sockaddr *)&cliaddr, &len);
-        //如果阻塞过程中被信号中断，某些系统可能会返回一个EINTR错误而不是重启accept
+        /* 如果阻塞过程中被信号中断，某些系统可能会返回一个EINTR错误而不是重启accept */
         if (confd < 0 && errno == EINTR) {
             continue;
         } else if (confd < 0) {
             printf("accept error,%s\n", strerror(errno));
             return 0;
         }
-
-        pid = fork();
-        //子进程
-        if (pid == 0) {
-            close(listenfd);
-            deal_socket(confd);
+        
+        /* 在子进程信息数组中找到当前空闲的进程，通过unix域套接字传递和客户端的已连接套接字的描述符 */
+        if (childs_run_num >= CHILDNUMBER) {
             close(confd);
-            return 0;
-        } else if (pid > 0) {
-        //父进程
-            close(confd);
-        } else {
-            printf("fork error\n");
+            printf("当前处理进程繁忙，无法处理新连接！已自动关闭连接！\n");
+            continue;
+        }
+        for (int i=0; i<CHILDNUMBER; i++) {
+            if (childs[i].is_use == 1 && childs[i].state == 0) {
+               //TODO...
+            }
         }
     }
     return 0;
