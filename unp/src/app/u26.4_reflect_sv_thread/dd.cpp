62f1f924e85b4a133a274f4f7a7cf5fb4d4a3928
fatedier <512654112@qq.com>
2014-11-26 23:09:37 +0800

    Add unp with threads

--- unp/src/app/u26.4_reflect_sv_thread/dd.cpp
+++ unp/src/app/u26.4_reflect_sv_thread/dd.cpp
@@ -1,28 +1,109 @@
 #include <stdio.h>
-#include <pthread.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
 
-int count = 0;
-pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
+#define MAXLINE 1024
 
+char ip[] = "0.0.0.0";
+int port = 9999;
 
-void *thread_main(void *ptr)
+int needprint = 0;
+
+//清除变成僵尸进程的子进程
+void clean_child(int signo)
 {
-    for (int i=0; i<100000; i++) {
-        pthread_mutex_lock(&count_mutex);
-        int temp = count;
-        printf("%d：%d\n", pthread_self(), temp + 1);
-        count = temp + 1;
-        pthread_mutex_unlock(&count_mutex);
+    pid_t pid;
+    int stat;
+    while ((pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+        if (needprint)
+            printf("child %d terminated\n", pid);
     }
-    return NULL;
+    return;
 }
 
-int main(int argc, char **argv)
+//处理连接的具体事务
+void deal_socket(int sockfd)
 {
-    pthread_t tid1, tid2;
-    pthread_create(&tid1, NULL, &thread_main, NULL);
-    pthread_create(&tid2, NULL, &thread_main, NULL);
-    pthread_join(tid1, NULL);
-    pthread_join(tid2, NULL);
+    char buf[MAXLINE];
+    int n;
+again:
+    while ((n = read(sockfd, buf, MAXLINE)) > 0) {
+        write(sockfd, buf, n);
+    }
+
+    if (n < 0 && errno == EINTR)
+        goto again;
+    else if (n < 0)
+        printf("read error\n");
+}
+
+int main()
+{
+    //绑定SIGCHLD信号的处理函数
+    signal(SIGCHLD, clean_child);
+
+    int listenfd, confd;
+
+    struct sockaddr_in svaddr;
+    svaddr.sin_family = AF_INET;
+    svaddr.sin_addr.s_addr = inet_addr(ip);
+    svaddr.sin_port = htons(port);
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
+        printf("create error,%s\n", strerror(errno));
+        return 0;
+    }
+    
+    int reuse = 1;
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        printf("setsockopt error,%s\n", strerror(errno));
+        return 0;
+    }
+
+    if (bind(listenfd, (struct sockaddr *)&svaddr, sizeof(svaddr)) < 0) {
+        printf("bind error,%s\n", strerror(errno));
+        return 0;
+    }
+
+    if (listen(listenfd, 50) < 0) {
+        printf("listen error,%s\n", strerror(errno));
+        return 0;
+    }
+
+    pid_t pid;
+    struct sockaddr_in cliaddr;
+    socklen_t len;
+    len = sizeof(cliaddr);
+    for (;;) {
+        confd = accept(listenfd, (struct sockaddr *)&cliaddr, &len);
+        //如果阻塞过程中被信号中断，某些系统可能会返回一个EINTR错误而不是重启accept
+        if (confd < 0 && errno == EINTR) {
+            continue;
+        } else if (confd < 0) {
+            printf("accept error,%s\n", strerror(errno));
+            return 0;
+        }
+
+        pid = fork();
+        //子进程
+        if (pid == 0) {
+            close(listenfd);
+            deal_socket(confd);
+            close(confd);
+            return 0;
+        } else if (pid > 0) {
+        //父进程
+            close(confd);
+        } else {
+            printf("fork error\n");
+        }
+    }
     return 0;
 }
