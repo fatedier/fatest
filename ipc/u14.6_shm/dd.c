9edd9a68af9a79ecc980179740da77c5b5fa340f
fatedier <512654112@qq.com>
2014-12-02 17:54:36 +0800

    Add share memory

---
+++ ipc/u14.6_shm/dd.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+#define MAX_DATA 1024
+#define SHM_KEY 1
+
+void show_help()
+{
+    printf("Usage: -s [command] -c [content]\n");
+    printf("    -s    use create,write,read,close to start with different function\n");
+    printf("    -c    with \"-s write\" set the send message content\n");
+}
+
+int main(int arc, char **argv)
+{
+    char start_cmd[20] = {0};       /* 启动命令 */
+    char content[MAX_DATA] = {0};   /* 内容字符串 */
+    
+    char *ptr;  /* 共享内存空间地址 */
+    
+    int c;
+    while ((c = getopt(argc, argv, "s:c:")) != -1) {
+        switch (c) {
+        case 's':
+            strncpy(start_cmd, optarg, sizeof(start_cmd));
+            break;
+
+        case 'c':
+            strncpy(content, optarg, sizeof(content));
+            break;
+
+        case '?':
+            show_help();
+            return -1;
+        }
+    }
+
+    if (optind == 1) {
+        show_help();
+        return -1;
+    }
+
+    if (strcmp(start_cmd, "") == 0) {
+        printf("error: please use -s option to set start_cmd\n");
+        return -1;
+    } else if (strcmp(start_cmd, "create") == 0) {
+    /* 创建共享内存 */
+        int shm_id = shmget(SHM_KEY, MAX_DATA, 0640 | IPC_CREATE);
+        ptr = shmat(shm_id, NULL, 0);
+    } else if (strcmp(start_cmd, "close") == 0) {
+    /* 关闭共享内存 */
+        int shm_id = shmget(SHM_KEY, 0, 0640 | IPC_CREATE);
+        shmctl(shm_id, IPC_RMID, NULL);
+    } else if (strcmp(start_cmd, "write") == 0) {
+        
+    }
+    return 0;
+}
